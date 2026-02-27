0696767916ef4a1988ecda657c14fce7e0c57da4
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-02-27 23:42:51 +0530

    Add DashboardMain layout and update controller; implement button actions for navigation

--- src/main/java/edu/icet/controller/dashboard/DashboardFormController.java
+++ src/main/java/edu/icet/controller/dashboard/DashboardFormController.java
@@ -1,4 +1,75 @@
 package edu.icet.controller.dashboard;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class DashboardFormController {
+
+    @FXML
+    private Button btnCustomer;
+
+    @FXML
+    private Button btnCustomers;
+
+    @FXML
+    private Button btnDashboard;
+
+    @FXML
+    private Button btnMedicine;
+
+    @FXML
+    private Button btnOrders;
+
+    @FXML
+    private Button btnPOS;
+
+    @FXML
+    private Button btnSupplier;
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCustomersOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnHelpOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnMedicineOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrdersOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPOSOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierOnAction(ActionEvent event) {
+
+    }
+
 }
