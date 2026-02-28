3587e0a64cde831bc2f507ff38896008db012edc
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-03-01 02:10:31 +0530

    Update Medicine view layout and integrate with Starter class

--- src/main/java/edu/icet/Starter.java
+++ src/main/java/edu/icet/Starter.java
@@ -11,7 +11,7 @@ public class Starter extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/DashboardMain.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Medicine.fxml"))));
         stage.show();
     }
 }
