7394c334968e1067cc5c5829d9116390cd93255f
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-03-01 01:15:21 +0530

    Implement CRUD methods in MedicineRepository and MedicineRepositoryImpl

--- src/main/java/edu/icet/repository/impl/MedicineRepositoryImpl.java
+++ src/main/java/edu/icet/repository/impl/MedicineRepositoryImpl.java
@@ -1,4 +1,34 @@
 package edu.icet.repository.impl;
 
-public class MedicineRepositoryImpl {
+import edu.icet.model.dto.Medicine;
+import edu.icet.repository.MedicineRepository;
+
+import java.util.Date;
+import java.util.List;
+
+public class MedicineRepositoryImpl implements MedicineRepository {
+    @Override
+    public void addMedicine(Integer id, String name, String brand, String category, Date expiryDate, Integer quantity, Double unitPrice, Double buyingPrice, Integer supplierId, String batchNumber, Integer reorderLevel, Date createdAt) {
+
+    }
+
+    @Override
+    public void updateMedicine(Integer id, String name, String brand, String category, Date expiryDate, Integer quantity, Double unitPrice, Double buyingPrice, Integer supplierId, String batchNumber, Integer reorderLevel, Date createdAt) {
+
+    }
+
+    @Override
+    public void deleteMedicine(Integer id) {
+
+    }
+
+    @Override
+    public Medicine searchMedicineById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Medicine> getAllMedicines() {
+        return List.of();
+    }
 }
