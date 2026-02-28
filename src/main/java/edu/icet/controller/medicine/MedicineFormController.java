1e3d6ff3f09fadb39358cd83f4becbdf808f5e3b
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-03-01 02:18:36 +0530

    Add FXML IDs and event handlers for Medicine form components

--- src/main/java/edu/icet/controller/medicine/MedicineFormController.java
+++ src/main/java/edu/icet/controller/medicine/MedicineFormController.java
@@ -1,4 +1,66 @@
 package edu.icet.controller.medicine;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+
 public class MedicineFormController {
+    @FXML
+    private Button btnAddNewMedicine;
+
+    @FXML
+    private TableColumn<?, ?> colBatchNumber;
+
+    @FXML
+    private TableColumn<?, ?> colBrand;
+
+    @FXML
+    private TableColumn<?, ?> colBuyingPrice;
+
+    @FXML
+    private TableColumn<?, ?> colCategory;
+
+    @FXML
+    private TableColumn<?, ?> colCreatedAt;
+
+    @FXML
+    private TableColumn<?, ?> colExpiryDate;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colQuantity;
+
+    @FXML
+    private TableColumn<?, ?> colReOrderLevel;
+
+    @FXML
+    private TableColumn<?, ?> colSupplierID;
+
+    @FXML
+    private TableColumn<?, ?> colUnitPrice;
+
+    @FXML
+    private Label lblRole;
+
+    @FXML
+    private TableView<?> tblMedicineDetails;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    void btnAddNewMedicineOnAction(ActionEvent event) {
+
+    }
+
 }
