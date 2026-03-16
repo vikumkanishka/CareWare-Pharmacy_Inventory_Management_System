7868356def7cfee5ca841145f4ead1f9a00fd25f
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-03-17 03:27:33 +0530

    Add UI components and event handlers to CustomerFormController

--- src/main/java/edu/icet/controller/customer/CustomerFormController.java
+++ src/main/java/edu/icet/controller/customer/CustomerFormController.java
@@ -1,4 +1,73 @@
 package edu.icet.controller.customer;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class CustomerFormController {
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colCreatedAt;
+
+    @FXML
+    private TableColumn<?, ?> colEmail;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colPhone;
+
+    @FXML
+    private DatePicker dateCreatedAt;
+
+    @FXML
+    private Label lblRole;
+
+    @FXML
+    private TableView<?> tblCustomerDetails;
+
+    @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtCustomerId;
+
+    @FXML
+    private JFXTextField txtCustomerName;
+
+    @FXML
+    private JFXTextField txtPhone;
+
+    @FXML
+    private JFXTextField txtSearch;
+
+    @FXML
+    private JFXTextField txtxCustomerEmail;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
 }
