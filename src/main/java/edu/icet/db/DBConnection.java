0ab3c11df0bd366ac13ba665630c67d450d9d846
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-03-16 22:30:46 +0530

    Implement singleton pattern for database connection management

--- src/main/java/edu/icet/db/DBConnection.java
+++ src/main/java/edu/icet/db/DBConnection.java
@@ -1,4 +1,28 @@
 package edu.icet.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+
+    private static DBConnection instance;
+
+    private Connection connection;
+
+    private DBConnection() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/CareWare", "root", "200004602360");
+    }
+
+    public static DBConnection getInstance() throws SQLException {
+        if (instance == null){
+            instance = new DBConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
 }
